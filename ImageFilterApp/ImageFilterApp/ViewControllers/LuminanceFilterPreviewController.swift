460d881d14605237f146a13d9173458f1aa2b72f
g4zeru <haru25haru02@gmail.com>
2019-02-26 14:10:04 +0900

    fix: Preview

--- ImageFilterApp/ImageFilterApp/ViewControllers/LuminanceFilterPreviewController.swift
+++ ImageFilterApp/ImageFilterApp/ViewControllers/LuminanceFilterPreviewController.swift
@@ -37,10 +37,7 @@ class LuminanceFilterPreviewController: BaseViewController {
         self.view.addSubview(self.preview)
         self.view.addSubview(self.luminanceSlider)
         self.didChangeShapenLuminanceValue()
-        let filter = TestFilter2()
-        filter.inputImage = CIImage(image: originImage!)
-        self.preview.image = self.filter.createImage(filter: filter)
-        //self.luminanceSlider.addTarget(self, action: #selector(didChangeShapenLuminanceValue), for: UIControl.Event.valueChanged)
+        self.luminanceSlider.addTarget(self, action: #selector(didChangeShapenLuminanceValue), for: UIControl.Event.valueChanged)
         self.setupLayer()
     }
     
